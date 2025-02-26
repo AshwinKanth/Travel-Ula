d3146fe8bcaa383337af71e3439bc2d312b2c4d0
AshwinKanth <ashwinvjrj@gmail.com>
2025-02-26 16:20:50 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import MaasaiMaraLanding from "./components/MaasaiMaraLanding";
 import Footer from "./components/Footer";
+
 import "./App.css"
 
 const App = () =>(
